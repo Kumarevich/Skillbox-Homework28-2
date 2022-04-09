52fdd41f417c6a76571f9bd78cec3ebdf36b21c1
Igor Makarevich <igor.makarevich@novometgroup.com>
2022-04-09 21:22:40 +0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+void train_station(char name)
+{
+    std::string command;
+    std::cout << "Train " << name << " has arrived on the station." << std::endl;
+    while (command != "depart")
+    {
+        std::cout << "Input command for train " << name << ": ";
+        std::cin >> command;
+    }
+    std::cout << "Train " << name << " is departing." << std::endl;
+}
+std::mutex train_station_access;
+
+void train_track(int timeSec, char name)
+{
+    std::this_thread::sleep_for(std::chrono::seconds(timeSec));
+    train_station_access.lock();
+    train_station(name);
+    train_station_access.unlock();
+}
+
+int main() {
+    int trainA, trainB, trainC;
+    std::cout << "Input times for trains A, B and C: ";
+    std::cin >> trainA >> trainB >> trainC;
+    std::thread train_track_A(train_track, trainA, 'A');
+    std::thread train_track_B(train_track, trainB, 'B');
+    std::thread train_track_C(train_track, trainC, 'C');
+    train_track_A.join();
+    train_track_B.join();
+    train_track_C.join();
+    return 0;
+}
